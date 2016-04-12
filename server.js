43245475fa9ff531e2b793fce59cb3da7fa8913c
tal cohen <tal cohen>
2016-04-12 18:15:27 +0300

    Added all the sources

---
+++ server.js
@@ -0,0 +1,22 @@
+/*var http = require('http');
+
+http.createServer(function(request, response){
+    response.end("Hello");
+}).listen(8080, function(){
+    console.log("on 8080");
+});*/
+
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/user', function(request,response){
+    /*var name = request.query.name;
+    var age = request.query.age;
+    var id = request.query.id;
+    response.end("Hello, "+ name + ". Your age is- " + age);*/
+    response.sendfile(path.join(__dirname, '/views/index.html'));
+});
+app.listen(8080);
